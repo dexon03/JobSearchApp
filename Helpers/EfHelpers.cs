ef37999a89cac7d29496720d7f6d2e0125e2681c
dexon03 <dima0958757803@gmail.com>
2025-05-21 09:14:08 +0300

    added setup for tests

---
+++ Helpers/EfHelpers.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Moq;
+
+namespace Helpers;
+
+public class EfHelpers
+{
+    public static Mock<DbSet<T>> CreateMockDbSet<T>(IQueryable<T> data) where T : class
+    {
+        var mockSet = new Mock<DbSet<T>>();
+
+        mockSet.As<IQueryable<T>>().Setup(m => m.Provider).Returns(data.Provider);
+        mockSet.As<IQueryable<T>>().Setup(m => m.Expression).Returns(data.Expression);
+        mockSet.As<IQueryable<T>>().Setup(m => m.ElementType).Returns(data.ElementType);
+        mockSet.As<IQueryable<T>>().Setup(m => m.GetEnumerator()).Returns(data.GetEnumerator());
+
+        mockSet.As<IAsyncEnumerable<T>>().Setup(m => m.GetAsyncEnumerator(It.IsAny<CancellationToken>()))
+            .Returns(new TestAsyncEnumerator<T>(data.GetEnumerator()));
+
+        mockSet.As<IQueryable<T>>().Setup(m => m.Provider)
+            .Returns(new TestAsyncQueryProvider<T>(data.Provider));
+
+        return mockSet;
+    }
+    
+    public static Mock<DbSet<T>> CreateMockDbSet<T>(List<T> data) where T : class
+        => CreateMockDbSet(data.AsQueryable());
+
+}
---
+++ Helpers/TestAsyncQueryProvider.cs
@@ -0,0 +1,41 @@
+﻿using System.Linq.Expressions;
+using Microsoft.EntityFrameworkCore.Query;
+
+namespace Helpers;
+
+public class TestAsyncQueryProvider<T> : IAsyncQueryProvider
+{
+    private readonly IQueryProvider _inner;
+
+    public TestAsyncQueryProvider(IQueryProvider inner)
+    {
+        _inner = inner;
+    }
+
+    public IQueryable CreateQuery(Expression expression) => _inner.CreateQuery(expression);
+
+    public IQueryable<TElement> CreateQuery<TElement>(Expression expression)
+        => new TestAsyncEnumerable<TElement>(expression);
+
+    public object Execute(Expression expression)
+        => _inner.Execute(expression);
+
+    public TResult Execute<TResult>(Expression expression)
+    {
+        var isTask = typeof(TResult).IsGenericType && typeof(TResult).GetGenericTypeDefinition() == typeof(Task<>);
+
+        if (isTask)
+        {
+            var resultType = typeof(TResult).GetGenericArguments()[0];
+            var executionResult = _inner.Execute(Expression.Convert(expression, resultType));
+            return (TResult)typeof(Task).GetMethod(nameof(Task.FromResult))!
+                .MakeGenericMethod(resultType)
+                .Invoke(null, new[] { executionResult })!;
+        }
+
+        return _inner.Execute<TResult>(expression);
+    }
+
+    public TResult ExecuteAsync<TResult>(Expression expression, CancellationToken cancellationToken)
+        => Execute<TResult>(expression);
+}
